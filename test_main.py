4d435fa306df2a2eaca195fd71a043743c4a3683
Swaroop <141357728+swar41@users.noreply.github.com>
2023-11-17 00:22:42 +0530

    Add files via upload

---
+++ test_main.py
@@ -0,0 +1,29 @@
+import selenium
+import os
+import time
+
+
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = selenium.webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+def testurl():
+    driver.get(os.getenv('BASEURL'))
+    driver.maximize_window()
+    page_url: str = driver.current_url
+    assert os.getenv('BASEURL') in page_url
+
+    
+def testtitle():
+    assert 'COMPRESS MASTER' == driver.title, 'unexpected title'
+
+
+def testcred():
+    driver.find_element(By.CSS_SELECTOR, '#file-type').send_keys('FILETYPE')
+    driver.find_element(By.CSS_SELECTOR, '#parameter').send_keys('parameter')
+
+
+time.sleep(10)
