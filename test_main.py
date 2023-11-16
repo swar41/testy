ae101565bbe0e3177b70f275ec0383f075c2ef94
Swaroop <141357728+swar41@users.noreply.github.com>
2023-11-17 00:53:38 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = selenium.webdriver.Chrome(service=ChromeDriverManager().install())
+driver = selenium.webdriver.Chrome()
 
 
 def testurl():
