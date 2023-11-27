ba46fa674c243ef3292669d34f93c1551fd7aed8
Swaroop <141357728+swar41@users.noreply.github.com>
2023-11-27 10:38:42 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
-from webdriver_manager.utils import ChromeType
 
 
 
